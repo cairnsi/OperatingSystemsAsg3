4860f2f32c41f9681f9113eec28471da4169b542
Ian Cairns <cairnsi@os1.engr.oregonstate.edu>
2020-10-24 17:15:37 -0700

    creating command struct

--- main.c
+++ main.c
@@ -12,15 +12,73 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
-#include <unistd.h>   // for execv
+#include <unistd.h>   
 
-struct movie
+#define MAXINPUT 2048
+
+struct commandLine
 {
-    char* command;
     char** arguments;
-    double rating;
-    struct movie* next;
+    bool background;
+    char* inputFile;
+    char* outputFile;
 };
+
+/* Parse the current line
+*/
+struct commandLine* createCommand(char* currLine)
+{
+    struct commandLine* command = malloc(sizeof(struct commandLine));
+
+    // For use with strtok_r
+    char* saveptr;
+
+    //create memory for arguments; 512 + 1 for the command and 1 for null;
+    command->arguments = calloc(514, sizeof(char*));
+    command->background = false;
+    command->inputFile = NULL;
+    command->outputFile = NULL;
+    int index = 0;
+
+    //get rid of trailing new line
+    currLine[strlen(currLine) - 1] = 0;
+    // Get the first element
+    char* token = strtok_r(currLine, " ", &saveptr);
+
+    while (token != NULL) {
+        //check for input output and background chars
+        if (strcmp(token, "<") == 0) {
+            //get the file name and set it as the input file
+            token = strtok_r(NULL, " ", &saveptr);
+            command->inputFile = calloc(strlen(token) + 1, sizeof(char));
+            strcpy(command->inputFile, token);
+        }
+        else if (strcmp(token, ">") == 0) {
+            //get the file name and set it as the output file
+            token = strtok_r(NULL, " ", &saveptr);
+            command->outputFile = calloc(strlen(token) + 1, sizeof(char));
+            strcpy(command->outputFile, token);
+        }
+        else if (strcmp(token, "&") == 0) {
+            //set the background boolean
+            command->background = true;
+        }
+        else {
+            // if not tags caught, then it is an argument. Add it to the arguments array
+            command->arguments[index] = calloc(strlen(token) + 1, sizeof(char));
+            strcpy(command->arguments[index], token);
+            index++;
+        }
+
+        // get next portion of command
+        token = strtok_r(NULL, " ", &saveptr);
+    }
+
+    //make last argument NULL
+    command->arguments[index] = NULL;
+
+    return command;
+}
 
 /*
 *       main function for smallsh terminal
@@ -32,20 +90,17 @@ int main(int argc, char* argv[])
 {
     bool askAgain = true;
     //run outer loop asking if user would like to specify a file
-    /*do {
+    do {
         printf(": ");
-        char* input = calloc(2048, sizeof(char));
-        scanf("%s", input);
-        
+        //allocate max input plus 1 for null at the end
+        char* input = calloc(MAXINPUT+1, sizeof(char));
+        fgets(input, MAXINPUT, stdin);
+        struct commandLine* command = createCommand(input);
 
+        askAgain = false;
         free(input);
-    } while (askAgain);*/
+    } while (askAgain);
     
-    char* newargv[] = { "/bin/echo", "pwd", "(10", "points", "for being in the HOME dir)", NULL };
-    execv(newargv[0], newargv);
-    /* exec returns only on error */
-    perror("execv");
-    exit(EXIT_FAILURE);
 
     return EXIT_SUCCESS;
 }
